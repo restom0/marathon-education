6ffe83d07142c24dca2a112d7ff01d0d11a2f104
restom0 <98004370+restom0@users.noreply.github.com>
2023-09-09 20:16:26 +0700

    lab-2

--- course-2/lab-1/src/components/Home.jsx
+++ course-2/lab-1/src/components/Home.jsx
@@ -1,30 +1,103 @@
 import React, { useState } from 'react';
 import Navbar from './Navbar';
 import Container from 'react-bootstrap/Container';
-
+import Swal  from "sweetalert2";
 function Home(props) {
     const [todo, setTodo] = useState([]);
     const [item, setItem] = useState([]);
+    const todoList = todo.map((item, index) =>
+        <tr>
+            <th>{++index}</th>
+            <td>{item.todo}</td>
+            <td>{
+                item.status === 0 ?
+                    <input type="checkbox" onChange={(e)=>updateTodo(index-1,e)} name="" id="" />
+                    :
+                    <input type="checkbox" checked disabled name="" id="" />
+            }</td>
+            <td><button className='btn btn-danger' onClick={() => deleteTodo(item.id)}>Xóa</button></td>
+        </tr>
+    )
+
     const addTodo = () => {
-        setTodo(todo => [...todo, item]);
+        var item1 = new Object();
+        item1.id = Date.now();
+        item1.todo = item;
+        item1.status = 0;
+        setTodo(todo => [...todo, item1]);
+    }
+    const deleteTodo = (i) => {
+        Swal.fire({
+            icon:'question',
+            title: 'Xóa todo ?',
+            showDenyButton: true,
+            showCancelButton: false,
+            confirmButtonText: 'Xóa',
+            denyButtonText: `Không`,
+          }).then((result) => {
+            if (result.isConfirmed) {
+                setTodo((current) =>
+                current.filter((todo) => todo.id !== i))
+            } else if (result.isDenied) {
+              Swal.fire('Changes are not saved', '', 'info')
+            }
+          })
+        
+    }
+    const updateTodo = (i,e) => {
+        todo[i]['status'] =1;
+        const todoList = todo.map((item, index) =>
+        <tr>
+            <th>{++index}</th>
+            <td>{item.todo}</td>
+            <td>{
+                item.status === 0 ?
+                    <input type="checkbox" onChange={(e)=>updateTodo(index-1,e)} name="" id="" />
+                    :
+                    <input type="checkbox" checked disabled name="" id="" />
+            }</td>
+            <td><button className='btn btn-danger' onClick={() => deleteTodo(item.id)}>Xóa</button></td>
+        </tr>
+    )
     }
     return (
         <div>
             <Navbar name={props.name} />
             <Container>
                 <div className='mt-4 row w-100'>
-                    <div className="col-md-8">
-                        <input type="text" className="form-control" placeholder='Todo' onKeyUp={(e) => setItem(e.target.value)} id="" />
-                    </div>
-                    <div className="col-md">
-                        <button className='btn btn-primary w-50' onClick={addTodo}>Thêm</button>
+                    <div className='row mt-3 ms-3'>
+                        <div className="col-md-8">
+                            <input type="text" className="form-control" placeholder='Todo' onChange={(e) => setItem(e.target.value)} id="" />
+                        </div>
+                        <div className="col-md">
+                            <button className='btn btn-primary w-50' onClick={addTodo}>Thêm</button>
+                        </div>
                     </div>
                     <div className="row mt-3 ms-3">
-                        {todo && todo.map(item => (
+                        {/* {todo && todo.map(item => (
                             <li key={item.id}>
                                 {item}
                             </li>
-                        ))}
+                        ))} */}
+                        {/* {todo && todo.length > 0 &&
+                            <ul>
+                                {todoList}
+                            </ul>
+                        } */}
+                        {todo && (
+                            <table className="table">
+                                <thead className="table-dark">
+                                    <tr>
+                                        <th scope="col">#</th>
+                                        <th scope="col">Todo</th>
+                                        <th scope="col">Tình trạng</th>
+                                        <th scope="col">Tùy chỉnh</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {todoList}
+                                </tbody>
+                            </table>)}
                     </div>
                 </div>
             </Container>
--- course-2/lab-2/src/App.js
+++ course-2/lab-2/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from './components/Home';
 import News from "./components/News";
 import About from "./components/About";
+import Login from "./components/Login";
 function App() {
   return (
     <div className="App">
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home name='dsa' />} />
           <Route exact path="/news" element={<News />} />
+          <Route exact path="/login" element={<Login />} />
           <Route path="/about" element={<About />} />
         </Routes>
       </BrowserRouter>
